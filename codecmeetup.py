42234b404a1c2227b04e03b9038cd524238c98ac
Kotoz <mohamedtareck95@gmail.com>
2017-04-24 18:00:55 +0200

    python training breif file

---
+++ codecmeetup.py
@@ -0,0 +1,23 @@
+#python quotations 
+#'character' , "Word" , 
+#""" Multiply lines  paragraph"""
+##############################
+#formate strings %s %d %f 
+#example 
+list =["Codec","First"]
+letter = """ Dear %s team \n this is our %s meetup 
+
+"""
+print letter%(list[0],list[1])
+###############################
+#Python supports the if, else, and elif statements for conditional execution of code. The syntax is if
+#expression: block. If the expression evaluates to true execute the block of code
+x= False
+y = False
+if x == True : 
+	print "X is true"
+elif y == True :
+	print"y is true" 
+else : 
+	print "Both false"
+######################################
