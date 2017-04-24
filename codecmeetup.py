0ffc1a280dd9e1f8423baf514ad9c34c989bccb5
Kotoz <mohamedtareck95@gmail.com>
2017-04-24 18:50:05 +0200

    breif

--- codecmeetup.py
+++ codecmeetup.py
@@ -10,8 +10,9 @@ letter = """ Dear %s team \n this is our %s meetup
 """
 print letter%(list[0],list[1])
 ###############################
-#Python supports the if, else, and elif statements for conditional execution of code. The syntax is if
-#expression: block. If the expression evaluates to true execute the block of code
+#Python supports the if, else, and elif statements for conditional execution of code. 
+#The syntax is >> if expression: block 
+#If the expression evaluates to true execute the block of code
 x= False
 y = False
 if x == True : 
@@ -21,3 +22,32 @@ elif y == True :
 else : 
 	print "Both false"
 ######################################
+#Python supports the while statement for conditional looping
+#the syntax is while expression: block.
+#While the expression evaluates to true, execute the block in looping fashion
+x= 1 
+while x< 10:
+	print x 
+	x +=1 
+#####################################
+#Python also supports the for statement for sequential looping. 
+#The syntax is for item in sequence:block. 
+#Each loop item is set to the next item in the sequence, and the block of code is executed. 
+#The for loop continues until there are no more items left in the sequence. 
+for x in range(1,10):
+	print "for loop " , x
+######### example 2 ############
+word = "Codec Team" 
+list =[] 
+for ch in word : 
+	list.append(ch)
+print list
+############# example 3 #######
+dic ={} 
+for i , char in enumerate(word):
+	dic[i]=char
+print dic 
+####################Example 4 ###############
+for key in dic:
+	print key ,'=', dic[key]
+#########################
